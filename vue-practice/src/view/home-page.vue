a1e9f1420573fa6a096abd4821865fdfd79f5c9e
wangm <2687804440@qq.com>
2019-09-30 17:17:03 +0800

    增加首页导航

---
+++ vue-practice/src/view/home-page.vue
@@ -0,0 +1,55 @@
+<template>
+    <div style="height: 100%;position: fixed;width:100%;">
+      <el-container  style="height:100%;border: 1px solid #eee">
+        <el-aside style="background-color: rgb(238, 241, 246);text-align: left;width:200px;">
+          <el-menu>
+            <el-submenu index="1">
+              <template slot="title"><i class="el-icon-message"></i>组件使用测试</template>
+              <el-menu-item-group>
+                <el-menu-item index="1-1" @click="goToSubPage('1-1')">alert使用</el-menu-item>
+                <el-menu-item index="1-2" @click="goToSubPage('1-2')">表格行列合并</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+            <el-submenu index="2">
+              <template slot="title"><i class="el-icon-message"></i>Vue</template>
+              <el-menu-item-group>
+                <el-menu-item index="2-1" @click="goToSubPage('2-1')">渲染函数</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+          </el-menu>
+        </el-aside>
+        <el-container style="width:100%;">
+          <el-main>
+            <router-view/>
+          </el-main>
+        </el-container>
+      </el-container>
+    </div>
+</template>
+
+<script>
+    export default {
+      name: "home-page",
+      data() {
+        return{}
+      },
+      mounted() {
+        this.$router.replace('table-row-merge');
+      },
+      methods:{
+        goToSubPage (position) {
+          if (position === '1-1') {
+            this.$router.replace('alert-test');
+          } else if (position === '1-2') {
+            this.$router.replace('table-row-merge');
+          } else if (position === '2-1') {
+            this.$router.replace('render-learning');
+          }
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
